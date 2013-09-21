6d259a583247a460f0046d6d06d870735a7888e9
georgefs <goblin.george@gmail.com>
2013-09-21 15:05:32 +0800

    add windows paste scripy

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import platform
 
-setup(name='pyclip',
-      version='0.1',
-      description='easily interact with clipboard from Python',
-      author='Zhehao Mao',
-      author_email='zhehao.mao@gmail.com',
-      url='https://github.com/zhemao/pyclip',
-      py_modules=['clipboard'],
-	  license='MIT'
+
+def build_params():
+    params = {
+      'name':'pyclip',
+      'version':'0.1',
+      'description':'easily interact with clipboard from Python',
+      'author':'Zhehao Mao',
+      'author_email':'zhehao.mao@gmail.com',
+      'url':'https://github.com/zhemao/pyclip',
+      'py_modules':['clipboard'],
+	  'license':'MIT',
+    }
+
+    #add windows paste command
+    if platform.system() == 'Windows':
+        params['scripts'] = ['paste.exe']
+
+setup(
+    **build_params()
 )
